4e08dc5823fe8fc704cb585ef3e6838d1635eaf5
delfinblaksleymujica <delf.blaksley@hotmail.com>
2023-04-08 21:27:17 -0300

    Sitio full responsive sin videos

--- js/clips.js
+++ js/clips.js
@@ -1,14 +1,14 @@
 const material = [
-    {id:1,nombre:"EN UNA",artista:"CAMILA BUCH",img:"../imagenesClips/1camibuch.jpg"/*,video:"../videosEnBaja/CamiBuch&MoraNavarroEnUna.mp4"*/},
-    {id:2,nombre:"NO SÉ",artista:"NIÑO ETC",img:"../imagenesClips/2-ninoetc.jpg"/*,video:"../videosEnBaja/NinioEtc.mp4"*/}, 
-    {id:4 ,nombre:"617",artista:"MUJER CEBRA",img:"../imagenesClips/4-mujercebra.jpg"/*,video:"../videosEnBaja/MujerCebra616.mp4"*/},
-    {id:5 ,nombre:"SILLÓN FREY",artista:"ESTUDIO TÉ",img:"../imagenesClips/5-estudiote.jpg"/*, video:"../videosEnBaja/SillonFrey.mp4"*/},
-    {id:6,nombre:"CERTEZAS",artista:"MORA NAVARRO",img:"../imagenesClips/6-certezas.jpg"/*,video:"../videosEnBaja/MoraNavarroCertezas.mp4"*/},
-    {id:7,nombre:"AGUJAS",artista:"PACO X WEREN",img:"../imagenesClips/7- agujas.jpg"/*,video:"../videos/PacoWerenAgujas.mp4"*/},
-    {id:8,nombre:"PEDRO RAUL",artista:"JERO JONES",img:"../imagenesClips/8-jerojones.jpg"/*,video:"../videos/JeroJonesPedroRaul.mp4"*/},
-    {id:9 ,nombre:"LA BRISA",artista:"PRADKIN",img:"../imagenesClips/9-brisa.jpg"/*,video:"../videos/PradkinLaBrisa.mp4"*/},
-    {id:10 ,nombre:"VAN AL CIELO",artista:"MARINA WILL",img:"../imagenesClips/10-marinawill.jpg"/*,video:"../videos/MarinaWilVanAlCielo.mp4"*/},
-    {id:11,nombre:"ALBEDRIO",artista:"PACO",img:"../imagenesClips/11-albedrio.jpg"/*,video:"../videos/PacoAlbedrio.mp4"*/},
+    {id:1,nombre:"EN UNA",artista:"CAMILA BUCH",img:"../imagenesClips/1camibuch.jpg",video:"../videosEnBaja/CamiBuch&MoraNavarroEnUna.mp4"},
+    {id:2,nombre:"NO SÉ",artista:"NIÑO ETC",img:"../imagenesClips/2-ninoetc.jpg",video:"../videosEnBaja/NinioEtc.mp4"}, 
+    {id:4 ,nombre:"617",artista:"MUJER CEBRA",img:"../imagenesClips/4-mujercebra.jpg",video:"../videosEnBaja/MujerCebra616.mp4"},
+    {id:5 ,nombre:"SILLÓN FREY",artista:"ESTUDIO TÉ",img:"../imagenesClips/5-estudiote.jpg", video:"../videosEnBaja/SillonFrey.mp4"},
+    {id:6,nombre:"CERTEZAS",artista:"MORA NAVARRO",img:"../imagenesClips/6-certezas.jpg",video:"../videosEnBaja/MoraNavarroCertezas.mp4"},
+    {id:7,nombre:"AGUJAS",artista:"PACO X WEREN",img:"../imagenesClips/7- agujas.jpg",video:"../videos/PacoWerenAgujas.mp4"},
+    {id:8,nombre:"PEDRO RAUL",artista:"JERO JONES",img:"../imagenesClips/8-jerojones.jpg",video:"../videos/JeroJonesPedroRaul.mp4"},
+    {id:9 ,nombre:"LA BRISA",artista:"PRADKIN",img:"../imagenesClips/9-brisa.jpg",video:"../videos/PradkinLaBrisa.mp4"},
+    {id:10 ,nombre:"VAN AL CIELO",artista:"MARINA WILL",img:"../imagenesClips/10-marinawill.jpg",video:"../videos/MarinaWilVanAlCielo.mp4"},
+    {id:11,nombre:"ALBEDRIO",artista:"PACO",img:"../imagenesClips/11-albedrio.jpg",video:"../videos/PacoAlbedrio.mp4"},
     ];
 
 
