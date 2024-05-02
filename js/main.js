a8583c57b9073c15c281d16627b9bac4abb62c87
delfinblaksleymujica <delf.blaksley@hotmail.com>
2024-05-02 18:56:27 -0300

    Sitio terminado

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const material = [
+    {id:1  , nombre:"CAMARÓN REMIX",artista:"JUKU ARES FT. COTI",img:"./imagenesClips/JukuFtCotiCamaron.webp",alt:"Portada de la canción Camarón de los artistas Juku Ares y Coti",title:"Hace click para ver el vídeo de la canción Camarón de los artistas Juku Ares y Coti",video:"https://www.youtube.com/embed/TXqQoIfnayY?si=Rgqej3mKY19oS6W7"},
     { id:2  , nombre:"AMARRE",artista:"RICA",img:"./imagenesFashionFilms/ElFilmRicaAmarre.webp",alt:"Portada del Fashion Film Amarre para Rica",title:"Hace click para ver el vídeo del fashion film Amarre para Rica",video:"https://www.youtube.com/embed/7eZV1UyNCXM?si=T7z0n8ucv8gw72Cx"},
     { id:3 , nombre:"CUANDO SEA GRANDE",artista:"MORA NAVARRO",img:"./imagenesClips/MoraNavarroCuandoSeaGrande.jpg",alt:"Portada de la canción Cuando Sea Grande de la artista Mora Navarro",title:"Hace click para ver el vídeo de la canción Cuando Sea Grande de la artista Mora Navarro",video:"https://www.youtube.com/embed/DjG-is23KTA?si=0B57b49r9oAyFz_x"},
     { id:4  , nombre:"SUEÑO AZUL",artista:"ABISMO",img:"./imagenesClips/SuenioAzulAbismo.jpg",alt:"Portada de la canción Sueño Azul de la banda musical Abismo",title:"Hace click para ver el vídeo de la canción Sueño Azul de la banda musical Abismo",video:"https://www.youtube.com/embed/zHXOWyp7HLU?si=j48H37o7e1Sv9wIW"},
@@ -124,7 +125,7 @@ function abrirVideo1Responsive() {
     cardsResponsiveContainer.classList.add("desaparecer");
     navBar.classList.add("desaparecer");
     div1Responsive.innerHTML = `<div class="video1__index__container">
-                                    <iframe src="https://www.youtube.com/embed/TXqQoIfnayY?si=Rgqej3mKY19oS6W7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                    <iframe src="https://www.youtube.com/embed/G6MyWa1yqFU?si=b3EZtqlkJ5qO8A6b" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                                     <button type="button" id="btnCerrar" onclick="cerrarVideo1Responsive()" class="btnCerrar" value="Cerrar">< Volver</button>
                                 </div>`
     document.body.appendChild( div1Responsive )
